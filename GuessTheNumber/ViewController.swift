c5d01a837c2516cdeb6d0f19564978b4fa16c99c
Mehmet Arikan <mehmetarikaan@outlook.com>
2022-07-15 10:33:00 +0300

    Initial Commit

---
+++ GuessTheNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessTheNumber
+//
+//  Created by Mehmet Arıkan on 15.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
